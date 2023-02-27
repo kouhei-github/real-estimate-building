a5de1ade9bc983ab72f27299ee1450ce6b185547
nagamatsu-k <nagamatsu-k@dym.jp>
2023-02-27 14:53:42 +0900

    ページネーションの作成

--- src/components/PageNation.vue
+++ src/components/PageNation.vue
@@ -1,18 +1,72 @@
 <script lang="ts" setup>
 interface Props {
-  currentPage: number;
+    totalCards: number
+    lastPage: number
+    onePerPage: number
 }
 
 const props = defineProps<Props>();
+
+const selectPageNation = () => {
+    if(currentPage.value < 5){
+        availableClickPages.value = [1,2,3,4,5]
+    } else if (currentPage.value === props.lastPage) {
+        availableClickPages.value = [currentPage.value-4, currentPage.value-3, currentPage.value-2, currentPage.value-1, currentPage.value ]
+    } else {
+        availableClickPages.value = [currentPage.value-2, currentPage.value-1, currentPage.value,currentPage.value+1,currentPage.value+2]
+    }
+}
+
+const route = useRoute();
+const { page } = route.query;
+const currentPage = ref<number>(typeof page === "undefined" ? 1 : Number(page))
+const availableClickPages = ref<Array<number>>([])
+selectPageNation()
+
+const router = useRouter()
+const selectPage = (page: number) => {
+    if (page > props.lastPage) return
+    router.push(`/?page=${String(page)}`)
+    currentPage.value = page
+    selectPageNation()
+}
+
+const movetToPage = (page: number) => {
+    if (page > props.lastPage) return
+    currentPage.value = currentPage.value + page
+    router.push(`/?page=${String(currentPage.value)}`)
+    selectPageNation()
+}
+
 
 </script>
 
 <template>
     <div class="full flex items-center justify-center">
-        <div class="font-bold"><span class="text-[#ed6103] text-lg">2,833</span>件</div>
-        <div>（総物件数：<span class=font-bold>13,678件</span>）</div>
-        <div>前へ</div>
-        <div v-for="(item, key) in "></div>
-        <div>次へ</div>
+        <div class="font-bold"><span class="text-[#ed6103] text-2xl">{{ props.totalCards }}</span>件</div>
+        <div
+            v-on:click="movetToPage(-1)"
+            class="border px-3 py-2 mx-2 hover:bg-gray-400 cursor-pointer border-gray-500"
+        >
+            前へ
+        </div
+>
+        <div
+            v-for="(item, key) in availableClickPages"
+            :key="key"
+            class="border border-gray-500 px-3 py-2 mx-2 hover:bg-gray-400 cursor-pointer"
+            :class="{ 'bg-[#ed6103] text-white hover:bg-[#ed6103]':  item === currentPage}"
+            v-on:click="selectPage(item)"
+        >
+            {{ item }}
+        </div>
+        <div class="border px-3 py-2 mx-2 text-gray-400">{{ lastPage }}</div>
+        <div
+            v-on:click="movetToPage(1)"
+            class="border border-gray-500 px-3 py-2 mx-2 hover:bg-gray-400 cursor-pointer"
+        >
+            次へ
+        </div
+>
     </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,23 @@ definePageMeta({
   layout: 'dashboard',
 })
 
+type PageNation = {
+    total     : number
+    lastPage  : number
+    onePerPage: number
+}
 
+const pageNations = reactive<PageNation>({
+    total: 0,
+    onePerPage: 0,
+    lastPage: 0,
+})
+
+onMounted(() => {
+    pageNations.total      = 2832
+    pageNations.onePerPage = 10
+    pageNations.lastPage   = Math.ceil(pageNations.total / pageNations.onePerPage)
+})
 
 </script>
 
@@ -15,7 +31,7 @@ definePageMeta({
     <div class="w-10/12 mx-auto">
       <FirstView />
       <div class="my-5">
-        <span class="text-orange-500 text-3xl font-extrabold">39,556</span><span class="text-orange-500 text-sm">件</span> 1～40件を表示
+        <span class="text-orange-500 text-3xl font-extrabold">{{ pageNations.total }}</span><span class="text-orange-500 text-sm">件</span> {{ `1～${pageNations.onePerPage}件を表示` }}
       </div>
 
       <div class="space-y-[50px]">
@@ -30,6 +46,14 @@ definePageMeta({
         <BuildingCard url="/about" building-name="マリオン川口第２ 西川口駅" />
         <BuildingCard url="/about" building-name="マリオン川口第２ 西川口駅" />
       </div>
+      <div class="w-w-full ml-auto">
+        <PageNation
+          :totalCards="pageNations.total"
+          :onePerPage="pageNations.onePerPage"
+          :lastPage="pageNations.lastPage"
+        />
+      </div>
+
     </div>
 
   </div>
